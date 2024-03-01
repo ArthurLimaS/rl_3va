ca23d299a6be1986760e659d70e68382b51d1679
Arthur Lima <als.arthurlimasilva@gmail.com>
2024-03-01 11:55:31 -0300

    ambiente atualizado para utilizar ações meta-discretas

--- aux_functions.py
+++ aux_functions.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
+from stable_baselines3.common.monitor import Monitor
 
-def intersection_v1(STACK_SIZE = 4):
+def intersection_v1(STACK_SIZE = 4, SCALING = 1):
 
     env = gym.make("intersection-v1", render_mode='rgb_array')
 
@@ -10,22 +11,29 @@ def intersection_v1(STACK_SIZE = 4):
             "observation_shape": (84, 84),
             "stack_size": STACK_SIZE,
             "weights": [0.2989, 0.5870, 0.1140],
-            "scaling": 1,
-        }
+            "scaling": SCALING,
+        },
+        "action": {
+            "type": "DiscreteMetaAction",
+            "longitudinal": False,
+            "lateral": True
+        },
     }
 
     env.unwrapped.configure(config)
 
-    return env
+    return Monitor(env)
 
 def render_model(model, env):
     obs, info = env.reset()
 
-    while True:
-        env.render()
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, terminated, truncated, info = env.step(action)
-        if terminated or truncated:
-            break
+    for _ in range(3):
+        while True:
+            env.render()
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = env.step(action)
+            if terminated or truncated:
+                obs, info = env.reset()
+                break
 
     env.close()
